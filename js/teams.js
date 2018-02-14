7db5d09be916be8aef112090511eec138ec80975
Chiefautoparts <chiefautoparts@outlook.com>
2018-02-14 11:22:14 -0800

    created repo 4

---
+++ js/teams.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var Conference = [];
+var East = [];
+var West = [];
+var Central = ['Chicago Deep Dishes', 'Detroit Dog Fighters', 'North Dakota Bison Herders', 'Kansas City CoonBears', 'Colorado Green Leafs', 'Toronto Rob Fords'];
+var Pacific = ['Seattle Fighting Salmon', 'San Diego Crips', 'San Fransico Sadomites', 'Saskatoon Saskatooners', 'Jueno Moose Jockeys', 'Prince Albert Rain Dancers'];
+var Southern = ['North Carolina Rocketmen', 'Tallahassee Swamp Kings', 'Miami King Pins', 'Atlantic City Fist Pumpers', 'New Orleans Tromboners', 'El Paso Nachoes'];
+var North_Eastern = ['Philadelphia Cheese Dogs', 'Pittsburgh Shark-Punchers', 'Boston Bar Brawlers', 'Buffalo Untamed Wings', 'New York Golden Rainbows', 'Washington Engines'];
+
+East.push(Southern);
+East.push(North_Eastern);
+West.push(Central);
+West.push(Pacific);
+
